f1c7d411d8febab28f6620050f29ba3b6f234db4
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2016-01-02 16:16:33 +0100

    [enh] enable camera widget and add center grid

---
+++ DrillCam.py
@@ -0,0 +1,41 @@
+import cv
+from VideoWidget import VideoWidget
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import QApplication, QImage, QPainter, QWidget
+from PyQt4.QtCore import QRectF
+
+
+class DrillCam(VideoWidget):
+    """ A class for rendering video coming from OpenCV and drawing a center grid """
+
+    def __init__(self, parent=None):
+        VideoWidget.__init__(self, parent)
+
+
+    def paintEvent(self, event):
+        VideoWidget.paintEvent(self, event)
+        qp = QPainter(self)
+        qp.setRenderHints(QPainter.Antialiasing)
+        #painter.begin(self)
+        qp.setPen(QtGui.QColor(255, 255, 0))
+        self.drawCenterGrid(qp)
+        qp.end()
+
+
+    def drawCenterGrid(self, qp):
+        size = self.size()
+        mid_x = size.width()/2
+        mid_y = size.height()/2
+
+        max_radius = min([size.width()/2, size.height()/2])
+        spacing = 40
+
+        for radius in range(20, max_radius, spacing):
+            qp.drawEllipse(mid_x-radius, mid_y-radius, 2*radius, 2*radius)
+            qp.drawLine(mid_x-radius-spacing/2, mid_y-spacing/2 , mid_x-radius-spacing/2, mid_y+spacing/2)
+            qp.drawLine(mid_x+radius+spacing/2, mid_y-spacing/2 , mid_x+radius+spacing/2, mid_y+spacing/2)
+            qp.drawLine(mid_x-spacing/2, mid_y-radius-spacing/2 , mid_x+spacing/2, mid_y-radius-spacing/2)
+            qp.drawLine(mid_x-spacing/2, mid_y+radius+spacing/2 , mid_x+spacing/2, mid_y+radius+spacing/2)
+
+        qp.drawLine(mid_x, 0 , mid_x, size.height())
+        qp.drawLine(0, mid_y , size.width(), mid_y)
--- VideoWidget.py
+++ VideoWidget.py
@@ -3,15 +3,16 @@ import sys
 
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import QPoint, QTimer
-from PyQt4.QtGui import QApplication, QImage, QPainter, QWidget
+from PyQt4.QtGui import QApplication, QImage, QPainter, QWidget, QMessageBox
 from PyQt4.QtCore import QObject, SIGNAL, SLOT, QPointF, QRectF, QPointF
+
 
 class IplQImage(QImage):
     """
     http://matthewshotton.wordpress.com/2011/03/31/python-opencv-iplimage-to-pyqt-qimage/
     A class for converting iplimages to qimages
     """
- 
+
     def __init__(self,iplimage):
         # Rough-n-ready but it works dammit
         alpha = cv.CreateMat(iplimage.height,iplimage.width, cv.CV_8UC1)
@@ -26,25 +27,32 @@ class IplQImage(QImage):
         ])
         self.__imagedata = rgba.tostring()
         super(IplQImage,self).__init__(self.__imagedata, iplimage.width, iplimage.height, QImage.Format_RGB32)
- 
- 
+
+
 class VideoWidget(QWidget):
     """ A class for rendering video coming from OpenCV """
- 
+
     def __init__(self, parent=None):
         QWidget.__init__(self)
-        self._capture = cv.CreateCameraCapture(0)
+        self._capture = cv.CaptureFromCAM(0)
+
         # Take one frame to query height
         frame = cv.QueryFrame(self._capture)
+        if not frame:
+            QMessageBox.information(self, "Could not open camera", "Please configure/enable camera");
+            return
+
         self.setMinimumSize(frame.width, frame.height)
         self.setMaximumSize(self.minimumSize())
         self._frame = None
         self._image = self._build_image(frame)
+
         # Paint every 50 ms
         self._timer = QTimer(self)
         self._timer.timeout.connect(self.queryFrame)
         self._timer.start(50)
- 
+
+
     def _build_image(self, frame):
         if not self._frame:
             self._frame = cv.CreateImage((frame.width, frame.height), cv.IPL_DEPTH_8U, frame.nChannels)
@@ -53,11 +61,13 @@ class VideoWidget(QWidget):
         else:
             cv.Flip(frame, self._frame, 0)
         return IplQImage(self._frame)
- 
+
+
     def paintEvent(self, event):
         painter = QPainter(self)
         painter.drawImage(QPoint(0, 0), self._image)
- 
+
+
     def queryFrame(self):
         frame = cv.QueryFrame(self._capture)
         self._image = self._build_image(frame)
