639f98547d27dbd6b3585ecb3dc01a04e3d39ea3
Friedrich Feichtinger <fritz_feichtiger@aon.at>
2016-01-08 09:41:12 +0100

    support imperial units in drill files

--- readDrillFile.py
+++ readDrillFile.py
@@ -17,16 +17,34 @@ def readDrillFile(filename):
 	tools={}
 	holes={}
 	
+	unit="metric"		# assume mm units by default
+	zeroMode="trailing"	# assume trailing zeros by default
+	
 	ID=0
 	
 	for line in drillfile:
 		
+		# read unit
+		if ("M71" in line) or ("METRIC" in line):
+			unit="metric"
+		if ("M72" in line) or ("INCH" in line):
+			unit="inch"
+		
+		# read zeroMode
+		if "LZ" in line:
+			zeroMode="leading"
+		if "TZ" in line:
+			zeroMode="trailing"
+		
+		
 		# read tool table
-		mo=re.search(r"^T(\d+)C(\d+\.\d+)$", line)
+		mo=re.search(r"^T(\d+)C(\d*\.\d+)$", line)
 		if mo:
 			#print mo.group()
 			toolNum=int(mo.group(1))
 			toolDia=float(mo.group(2))
+			if(unit=="inch"):
+				toolDia=toolDia*25.4
 			#print toolNum, toolDia
 			tools[toolNum]=toolDia
 			
@@ -37,16 +55,38 @@ def readDrillFile(filename):
 		mo=re.search(r"^T(\d+)$", line)
 		if mo:
 			currentTool=int(mo.group(1))
-			
 		
-		# read coordinates
-		mo=re.search(r"^X(-?\d+.\d+)Y(-?\d+.\d+)$", line)
+		# read coordinates (with decimal point)
+		mo=re.search(r"^X(-?\d+\.\d+)Y(-?\d+\.\d+)$", line)
 		if mo and currentTool in tools:
+			x=float(mo.group(1))
+			y=float(mo.group(2))
+			if unit=="inch":
+				x=x*25.4
+				y=y*25.4
+			holes[tools[currentTool]].add((x, y, ID))	
+		
+		# read coordinates (without decimal point)
+		mo=re.search(r"^X(-?\d+)Y(-?\d+)$", line)
+		if mo and currentTool in tools:
+			strX=mo.group(1)
+			strY=mo.group(2)	
+			if zeroMode=="leading":
+				strX=strX.ljust(6, '0')		# there are always 6 digits
+				strY=strY.ljust(6, '0')
 			
-			holes[tools[currentTool]].add((float(mo.group(1)), float(mo.group(2)), ID))
+			x=float(strX)
+			y=float(strY)
+			if(unit=="metric"):
+				x=x/1000		# metric format: 000.000
+				y=y/1000
+			elif(unit=="inch"):
+				x=x/10000*25.4	# inch format: 00.0000
+				y=y/10000*25.4
+				
+			holes[tools[currentTool]].add((x, y, ID))
 			
 		ID=ID+1
-	
 	
 	return holes
 
